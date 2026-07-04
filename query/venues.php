6a85ad6b1bf793693e1016367f8999c56df8da59
adamusann <adamiskhandar76@gmail.com>
2026-07-04 23:20:07 +0800

    buang error

--- query/venues.php
+++ query/venues.php
@@ -2,7 +2,7 @@
 
 header("Content-Type: application/json");
 require_once __DIR__ . "/../middleware/errorHandler.php.php";
->>>>>>> Stashed changes
+
 require_once __DIR__ . "/../config/db.php";
 require_once __DIR__ . "/../middleware/authMiddleware.php";
 
