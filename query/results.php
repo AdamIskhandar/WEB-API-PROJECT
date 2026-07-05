44ee68356e2470659c530f25664a47c2ecfdbac6
adamusann <adamiskhandar76@gmail.com>
2026-07-05 10:41:16 +0800

    delete double extension .php

--- query/results.php
+++ query/results.php
@@ -3,7 +3,7 @@
 header("Content-Type: application/json");
 
 
-require_once __DIR__ . "/../middleware/errorHandler.php.php";
+require_once __DIR__ . "/../middleware/errorHandler.php";
 require_once __DIR__ . "/../config/db.php";
 require_once __DIR__ . "/../middleware/authMiddleware.php";
 
--- query/venues.php
+++ query/venues.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Content-Type: application/json");
-require_once __DIR__ . "/../middleware/errorHandler.php.php";
+require_once __DIR__ . "/../middleware/errorHandler.php";
 
 require_once __DIR__ . "/../config/db.php";
 require_once __DIR__ . "/../middleware/authMiddleware.php";
